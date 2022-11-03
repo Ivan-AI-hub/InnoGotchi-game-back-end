da48aabd4bddca00959cfd48fa5a9c7cf9c0ebbf
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-04 00:35:25 +0300

    Create Pet class

---
+++ InnoGotchiGame/InnoGotchiGame.Domain/Pet.cs
@@ -0,0 +1,8 @@
+﻿namespace InnoGotchiGame.Domain
+{
+    public class Pet
+    {
+        public PetStatistic Statistic { get; set; }
+        public PetView View { get; set; }
+    }
+}
---
+++ InnoGotchiGame/InnoGotchiGame.Domain/PetStatistic.cs
@@ -0,0 +1,11 @@
+﻿namespace InnoGotchiGame.Domain
+{
+    public record PetStatistic
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int HappinessDaysCount { get; set; }
+        public DateOnly DataLastFeed { get; set; }
+        public DateOnly DataLastDrink { get; set; }
+    }
+}
