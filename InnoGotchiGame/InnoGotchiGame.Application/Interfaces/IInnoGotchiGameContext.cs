76e2800d4056c3d25f0cfec75b61b6eeb155acaf
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-20 18:40:21 +0300

    Create IInnoGotchiGameContext.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Interfaces/IInnoGotchiGameContext.cs
@@ -0,0 +1,14 @@
+﻿using InnoGotchiGame.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchiGame.Application.Interfaces
+{
+    public interface IInnoGotchiGameContext
+    {
+        DbSet<Pet> Pets { get; set; }
+        DbSet<PetFarm> PetFarms { get; set; }
+        DbSet<User> Users { get; set; }
+
+        public int SaveChanges();
+    }
+}
