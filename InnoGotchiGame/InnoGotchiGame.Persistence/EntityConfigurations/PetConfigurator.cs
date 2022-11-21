79470c284e38317af056bf7fbff055a411b7adfa
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-21 19:29:37 +0300

    Create PetConfigurator.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Persistence/EntityConfigurations/PetConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using InnoGotchiGame.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InnoGotchiGame.Persistence.EntityConfigurations
+{
+    internal class PetConfigurator : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.OwnsOne(p => p.View);
+            builder.OwnsOne(p => p.Statistic, cb =>
+            {
+                cb.HasIndex(x => x.Name).IsUnique();
+            });
+        }
+    }
+}
