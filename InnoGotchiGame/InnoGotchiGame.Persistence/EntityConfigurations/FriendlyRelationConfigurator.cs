26ab2f72c04b9e0bc415ff917542bc37c23cd524
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-23 15:49:58 +0300

    Create FriendlyRelationConfigurator.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Persistence/EntityConfigurations/FriendlyRelationConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using InnoGotchiGame.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchiGame.Persistence.EntityConfigurations
+{
+	internal class FriendlyRelationConfigurator : IEntityTypeConfiguration<FriendlyRelation>
+	{
+		public void Configure(EntityTypeBuilder<FriendlyRelation> builder)
+		{
+			builder.HasOne(d => d.FirstFriend)
+				.WithMany(p => p.SentFriendships)
+				.HasForeignKey(d => d.FirstFriendId)
+				.OnDelete(DeleteBehavior.NoAction);
+
+			builder.HasOne(d => d.SecondFriend)
+				.WithMany(p => p.AcceptedFriendships)
+				.HasForeignKey(d => d.SecondFriendId);
+		}
+	}
+}
