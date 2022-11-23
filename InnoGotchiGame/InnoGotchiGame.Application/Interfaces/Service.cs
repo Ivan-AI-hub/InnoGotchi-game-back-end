18129d7432059e6850b21f9ff13383fe7d9a9ab7
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-23 17:40:23 +0300

    Create Service.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Interfaces/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace InnoGotchiGame.Application.Interfaces
+{
+	public abstract class Service
+	{
+		protected IInnoGotchiGameContext Context { get; set; }
+		public Service(IInnoGotchiGameContext context)
+		{
+			Context = context;
+		}
+	}
+}
