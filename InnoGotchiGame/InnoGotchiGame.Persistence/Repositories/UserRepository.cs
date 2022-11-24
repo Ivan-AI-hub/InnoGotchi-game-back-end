96946f0b660033dac46d660d98871a633ca63daa
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-24 18:20:21 +0300

    Create UserRepository.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,75 @@
+﻿using InnoGotchiGame.Application.Interfaces;
+using InnoGotchiGame.Domain;
+using InnoGotchiGame.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchiGame.Application.Services
+{
+	public class UserService : IRepository<User>
+	{
+		private IInnoGotchiGameContext _context;
+		public UserService(IInnoGotchiGameContext context)
+		{
+			_context = context;
+		}
+
+		public User? GetItemById(int id)
+		{
+			return _context.Users.FirstOrDefault(x => x.Id == id);
+		}
+
+		public User? GetItem(Func<User, bool> predicate)
+		{
+			return _context.Users.FirstOrDefault(predicate);
+		}
+
+		public void Add(User item)
+		{
+			_context.Users.Add(item);
+		}
+
+		public void Update(int updatedId, User item)
+		{
+			item.Id = updatedId;
+			_context.Users.Update(item);
+		}
+
+		public void Delete(int id)
+		{
+			var user = GetItemById(id);
+			if (user != null)
+			{
+				_context.Users.Remove(user);
+			}
+		}
+
+		public IQueryable<User> GetItems(Func<User, bool>? whereRule = null, Func<User, dynamic>? orderByRule = null, bool isDescendingOrder = false)
+		{
+			var users = _context.Users
+				.Include(x => x.OwnPetFarm)
+				.Include(x => x.SentFriendships)
+				.Include(x => x.AcceptedFriendships);
+
+			if (whereRule != null)
+				users.Where(whereRule);
+
+			if (orderByRule != null)
+			{
+				if (isDescendingOrder)
+					users.OrderByDescending(orderByRule);
+				else
+					users.OrderBy(orderByRule);
+			}
+
+
+			return users;
+		}
+
+		public IQueryable<User> GetItemsPage(int pageSize, int pageNumber, Func<User, bool>? whereRule = null, Func<User, dynamic>? orderByRule = null, bool isDescendingOrder = false)
+		{
+			return GetItems(whereRule, orderByRule, isDescendingOrder)
+				.Skip(pageSize * (pageNumber - 1))
+				.Take(pageSize);
+		}
+	}
+}
