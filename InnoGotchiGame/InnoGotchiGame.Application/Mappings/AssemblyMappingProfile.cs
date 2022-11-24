1260ce7173347e1c0600136d4a35bb89669dbebc
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-25 00:53:39 +0300

    Create AssemblyMappingProfile.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using InnoGotchiGame.Application.Models;
+using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Mappings
+{
+	public class AssemblyMappingProfile : Profile
+	{
+		public AssemblyMappingProfile() 
+		{
+			CreateMap<User, UserDTO>().ReverseMap();
+			CreateMap<PetFarm, PetFarmDTO>().ReverseMap();
+			CreateMap<FriendlyRelation, FriendlyRelationDTO>().ReverseMap();
+			CreateMap<FriendlyRelationStatus, FriendlyRelationStatusDTO>().ReverseMap();
+
+			CreateMap<Pet, PetDTO>().ReverseMap();
+			CreateMap<PetStatistic, PetStatisticDTO>().ReverseMap();
+			CreateMap<PetView, PetViewDTO>().ReverseMap();
+		}
+	}
+}
