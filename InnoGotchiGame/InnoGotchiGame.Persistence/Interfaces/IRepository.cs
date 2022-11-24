a52662e7801111422639ce6240a9e27ecf4a7323
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-11-24 18:19:39 +0300

    Create IRepository.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Persistence/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Persistence.Interfaces
+{
+	internal interface IRepository<T> where T:class
+	{
+		public T? GetItemById(int id);
+
+		public T? GetItem(Func<T, bool> predicate);
+
+
+		public IQueryable<T> GetItems(Func<T, bool>? whereRule = null,
+										 Func<User, dynamic>? orderByRule = null,
+										 bool isDescendingOrder = false);
+
+		public IQueryable<T> GetItemsPage(int pageSize, int pageNumber,
+										 Func<T, bool>? whereRule = null,
+										 Func<T, dynamic>? orderByRule = null,
+										 bool isDescendingOrder = false);
+
+		public void Add(T item);
+
+		public void Update(int updatedId, T item);
+
+		public void Delete(int id);
+	}
+}
