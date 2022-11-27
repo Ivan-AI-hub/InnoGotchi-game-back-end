43bfc8f358cbac61c1262bd570d8bf75c96c02fa
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-11-27 21:57:25 +0300

    Create USerSorter

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Sorters/Sorter.cs
@@ -0,0 +1,9 @@
+﻿using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Sorters
+{
+	public abstract class Sorter<T>
+	{
+		internal abstract IQueryable<T> Sort(IQueryable<T> query);
+	}
+}
---
+++ InnoGotchiGame/InnoGotchiGame.Application/Sorters/UserSorter.cs
@@ -0,0 +1,45 @@
+﻿using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Sorters
+{
+	public class UserSorter : Sorter<User>
+	{
+		public bool IsFirstNameSort { get; set; }
+		public bool IsLastNameSort { get; set; }
+		public bool IsEmailSort { get; set; }
+		public bool IsDescendingSort { get; set; }
+
+		public UserSorter(bool isFirstNameSort = true, bool isLastNameSort = false, bool isEmailSort = false)
+		{
+			IsFirstNameSort = isFirstNameSort;
+			IsLastNameSort = isLastNameSort;
+			IsEmailSort = isEmailSort;
+			IsDescendingSort = false;
+		}
+
+
+
+		internal override IQueryable<User> Sort(IQueryable<User> users)
+		{
+			if(!IsDescendingSort)
+			{
+				if (IsFirstNameSort)
+					return users.OrderBy(x => x.FirstName);
+				else if (IsLastNameSort)
+					return users.OrderBy(x => x.LastName);
+				else if (IsEmailSort)
+					return users.OrderBy(x => x.Email);
+			}
+			else
+			{
+				if (IsFirstNameSort)
+					return users.OrderByDescending(x => x.FirstName);
+				else if (IsLastNameSort)
+					return users.OrderByDescending(x => x.LastName);
+				else if (IsEmailSort)
+					return users.OrderByDescending(x => x.Email);
+			}
+			return users;
+		}
+	}
+}
