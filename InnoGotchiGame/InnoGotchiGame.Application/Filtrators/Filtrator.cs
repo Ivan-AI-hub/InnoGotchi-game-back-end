ede13d4f482effc059258c7d102d751e74e93896
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-11-27 21:57:13 +0300

    Create UserFiltretor

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Filtrators/Filtrator.cs
@@ -0,0 +1,9 @@
+﻿using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Filtrators
+{
+	public abstract class Filtrator<T>
+	{
+		internal abstract IQueryable<T> Filter(IQueryable<T> query);
+	}
+}
---
+++ InnoGotchiGame/InnoGotchiGame.Application/Filtrators/UserFiltrator.cs
@@ -0,0 +1,28 @@
+﻿using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Filtrators
+{
+	public class UserFiltrator : Filtrator<User>
+	{
+		private string _firstName;
+		private string _lastName;
+		private string _email;
+		private int? _petFarmId;
+
+		public UserFiltrator(string firstName = "", string lastName = "", string email = "", int? petFarmId = null)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+			_email = email;
+			_petFarmId = petFarmId;
+		}
+
+		internal override IQueryable<User> Filter(IQueryable<User> users)
+		{
+			return users.Where(x => x.FirstName.Contains(_firstName) &&
+									x.LastName.Contains(_lastName) &&
+									x.Email.Contains(_email) &&
+									(_petFarmId == null || x.OwnPetFarmId == _petFarmId));
+		}
+	}
+}
