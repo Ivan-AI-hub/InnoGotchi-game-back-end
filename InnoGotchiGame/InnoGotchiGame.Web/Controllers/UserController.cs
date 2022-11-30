ea24a2553fc1cf119779aea8d26d3f8c6d6a8d3e
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-01 00:47:21 +0300

    Create UserController.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Web/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using InnoGotchiGame.Application.Managers;
+using InnoGotchiGame.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchiGame.Web.Controllers
+{
+	[Route("api/users")]
+	public class UserController : BaseController
+	{
+		private UserManager _manager;
+
+		public UserController(UserManager manager)
+		{
+			_manager = manager;
+		}
+
+		[HttpPost]
+		public IActionResult Post(UserDTO user)
+		{
+			if (user == null)
+				return BadRequest();
+
+			var rezult = _manager.Add(user);
+			if (!rezult.IsComplete)
+				return BadRequest();
+
+			return Ok(user);
+		}
+	}
+}
