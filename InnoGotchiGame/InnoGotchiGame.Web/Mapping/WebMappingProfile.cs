8f4b88817277de6567200630aa3f059a24eff823
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 00:20:12 +0300

    Create WebMappingProfile.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Web/Mapping/WebMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using InnoGotchiGame.Application.Models;
+using InnoGotchiGame.Web.Models.Users;
+
+namespace InnoGotchiGame.Web.Mapping
+{
+	public class WebMappingProfile : Profile
+	{
+		public WebMappingProfile()
+		{
+			CreateMap<AddUserModel, UserDTO>();
+			CreateMap<UpdateUserModel, UserDTO>();
+		}
+	}
+}
