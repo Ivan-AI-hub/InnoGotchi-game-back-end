51e8af083687abaac35b8e103898e55be3e5614f
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 16:46:34 +0300

    Create PetFarmValidator.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Validators/PetFarmValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Validators
+{
+	public class PetFarmValidator : AbstractValidator<PetFarm>
+	{
+		public PetFarmValidator() 
+		{
+			RuleFor(x => x.Name)
+				.NotEmpty()
+				.NotNull();
+
+			RuleFor(x => x.CreateDate)
+				.NotEmpty()
+				.NotNull();
+
+			RuleFor(x => x.OwnerId)
+				.NotNull()
+				.NotEmpty();
+		}
+	}
+}
