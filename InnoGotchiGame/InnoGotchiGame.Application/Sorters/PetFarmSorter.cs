d0d514d399120f0e62bda06d6891ed6499d57354
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 16:46:31 +0300

    Create PetFarmSorter.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Sorters/PetFarmSorter.cs
@@ -0,0 +1,31 @@
+﻿using InnoGotchiGame.Application.Sorters.Base;
+using InnoGotchiGame.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoGotchiGame.Application.Sorters
+{
+	public class PetFarmSorter : Sorter<PetFarm>
+	{
+		public bool IsNameSort { get; set; } = true;
+		public bool IsDescendingSort { get; set; } = false;
+
+		internal override IQueryable<PetFarm> Sort(IQueryable<PetFarm> petFarms)
+		{
+			if (!IsDescendingSort)
+			{
+				if (IsNameSort)
+					return petFarms.OrderBy(x => x.Name);
+			}
+			else
+			{
+				if (IsNameSort)
+					return petFarms.OrderByDescending(x => x.Name);
+			}
+			return petFarms;
+		}
+	}
+}
