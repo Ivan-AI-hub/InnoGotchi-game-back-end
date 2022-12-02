5f98a5e54e982907ce16d1bed18009028d803c43
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 13:49:07 +0300

    Add methods in requestManager

--- InnoGotchiGame/InnoGotchiGame.Application/Managers/ColaborationRequestManager.cs
+++ InnoGotchiGame/InnoGotchiGame.Application/Managers/ColaborationRequestManager.cs
@@ -18,10 +18,85 @@ namespace InnoGotchiGame.Application.Managers
 
 		public ManagerRezult SendColaborationRequest(int senderId, int recipientId)
 		{
+			var rezult = new ManagerRezult();
 			var request = new ColaborationRequest() { RequestSenderId = senderId, RequestReceiverId = recipientId, Status = ColaborationRequestStatus.Undefined};
-			_repository.Add(request);
-			_repository.Save();
-			return new ManagerRezult();
+			
+			try
+			{
+				_repository.Add(request);
+				_repository.Save();
+			}
+			catch(Exception ex)
+			{
+				if(ex.InnerException != null)
+					rezult.Errors.Add(ex.InnerException.Message);
+				rezult.Errors.Add(ex.Message);
+			}
+			return rezult;
+		}
+
+		public ManagerRezult ConfirmRequest(int requestId, int recipientId)
+		{
+			var rezult = new ManagerRezult();
+			var request = _repository.GetItemById(requestId);
+			if (request != null)
+			{
+				if (request.Status == ColaborationRequestStatus.Colaborators)
+					rezult.Errors.Add("Request already confirmed");
+				if (request.RequestReceiverId != recipientId)
+					rezult.Errors.Add("Only the recipient of the request can confirm the request. The recipient's ID does not match");
+				if(rezult.IsComplete)
+				{
+					request.Status = ColaborationRequestStatus.Colaborators;
+					_repository.Update(requestId, request);
+				}
+			}
+			else
+			{
+				rezult.Errors.Add("The request ID is not in the database");
+			}
+
+			return rezult;
+		}
+
+		public ManagerRezult RejectRequest(int requestId, int participantId)
+		{
+			var rezult = new ManagerRezult();
+			var request = _repository.GetItemById(requestId);
+			if (request != null)
+			{
+				if (request.Status == ColaborationRequestStatus.NotColaborators)
+					rezult.Errors.Add("Request already rejected");
+				if (request.RequestReceiverId != participantId && request.RequestSenderId != participantId)
+					rezult.Errors.Add("Only the participant of the request can reject the request. The recipient's ID does not match");
+				if (rezult.IsComplete)
+				{
+					request.Status = ColaborationRequestStatus.NotColaborators;
+					_repository.Update(requestId, request);
+				}
+			}
+			else
+			{
+				rezult.Errors.Add("The request ID is not in the database");
+			}
+
+			return rezult;
+		}
+
+		public ManagerRezult DeleteRequest(int requestId)
+		{
+			var rezult = new ManagerRezult();
+			var request = _repository.GetItemById(requestId);
+			if (request != null)
+			{
+				_repository.Delete(requestId);
+			}
+			else
+			{
+				rezult.Errors.Add("The request ID is not in the database");
+			}
+
+			return rezult;
 		}
 	}
 }
