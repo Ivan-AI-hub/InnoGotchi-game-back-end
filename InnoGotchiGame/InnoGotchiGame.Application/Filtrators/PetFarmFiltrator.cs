30798ee1620d57d75e71d4ac63007e74cfecaf7c
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 16:46:28 +0300

    Create PetFarmFiltrator.cs

---
+++ InnoGotchiGame/InnoGotchiGame.Application/Filtrators/PetFarmFiltrator.cs
@@ -0,0 +1,14 @@
+﻿using InnoGotchiGame.Application.Filtrators.Base;
+using InnoGotchiGame.Domain;
+
+namespace InnoGotchiGame.Application.Filtrators
+{
+	public class PetFarmFiltrator : Filtrator<PetFarm>
+	{
+		public string Name { get; set; } = "";
+		internal override IQueryable<PetFarm> Filter(IQueryable<PetFarm> query)
+		{
+			return query.Where(x => x.Name.Contains(Name));
+		}
+	}
+}
