38566dc0efa14147dcea6d02c19e03fd90179cd8
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-02 18:21:44 +0300

    Create PetFarmController

---
+++ InnoGotchiGame/InnoGotchiGame.Web/Controllers/PetFarmController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using InnoGotchiGame.Application.Filtrators;
+using InnoGotchiGame.Application.Managers;
+using InnoGotchiGame.Application.Models;
+using InnoGotchiGame.Application.Sorters;
+using InnoGotchiGame.Web.Models.PetFarms;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchiGame.Web.Controllers
+{
+	public class PetFarmController : BaseController
+	{
+		private PetFarmManager _farmManager;
+		private IMapper _mapper;
+
+		public PetFarmController(PetFarmManager farmManager, IMapper mapper)
+		{
+			_farmManager = farmManager;
+			_mapper = mapper;
+		}
+
+		[HttpPost]
+		public IActionResult Post(AddPetFarmModel addFarmModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+
+			PetFarmDTO farm = _mapper.Map<PetFarmDTO>(addFarmModel);
+
+			var rezult = _farmManager.Add(addFarmModel.OwnerId, farm);
+			if (!rezult.IsComplete)
+				return BadRequest(rezult.Errors);
+
+			return Ok(farm);
+		}
+
+		[HttpPut]
+		public IActionResult Put(UpdatePetFarmModel updatePetFarmModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+
+			var rezult = _farmManager.UpdateName(updatePetFarmModel.UpdatedId, updatePetFarmModel.Name);
+
+			if (!rezult.IsComplete)
+				return BadRequest(rezult.Errors);
+
+			return Ok();
+		}
+
+		[HttpGet]
+		public IActionResult Get(PetFarmFiltrator? filtrator = null, PetFarmSorter? sorter = null)
+		{
+			var farms = _farmManager.GetPetFarms(filtrator, sorter);
+			return new ObjectResult(farms);
+		}
+
+		[HttpGet("{farmId}")]
+		public IActionResult GetById(int farmId)
+		{
+			var farm = _farmManager.GetFarmById(farmId);
+			if (farm == null)
+				return BadRequest(new { errorText = "Invalid id." });
+
+			return new ObjectResult(farm);
+		}
+	}
+}
---
+++ InnoGotchiGame/InnoGotchiGame.Web/Models/PetFarms/AddPetFarmModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InnoGotchiGame.Web.Models.PetFarms
+{
+	public class AddPetFarmModel
+	{
+		public int OwnerId { get; set; }
+		public string Name { get; set; }
+	}
+}
