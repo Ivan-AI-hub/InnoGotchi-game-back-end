423d8f3e08f7d14e1654bd55d33076eb23fb3bc1
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2022-12-03 18:47:30 +0300

    Add methods into User

--- InnoGotchiGame/InnoGotchiGame.Domain/User.cs
+++ InnoGotchiGame/InnoGotchiGame.Domain/User.cs
@@ -1,4 +1,7 @@
-﻿namespace InnoGotchiGame.Domain
+﻿using System.Linq;
+using System.Net.Http.Headers;
+
+namespace InnoGotchiGame.Domain
 {
 	public class User
 	{
@@ -14,5 +17,29 @@
 
 		public List<ColaborationRequest> SentColaborations { get; set; }
 		public List<ColaborationRequest> AcceptedColaborations { get; set; }
+
+		public User()
+		{
+			SentColaborations = new List<ColaborationRequest>();
+			AcceptedColaborations = new List<ColaborationRequest>();
+		}
+
+		/// <returns>All colaborators of user</returns>
+		public IEnumerable<User> GetUserColaborators()
+		{
+			Func<ColaborationRequest, bool> whereFunc = x => x.Status == ColaborationRequestStatus.Colaborators;
+
+			List<User> friends = new List<User>();
+			friends.AddRange(AcceptedColaborations.Where(whereFunc).Select(x => x.RequestSender));
+			friends.AddRange(SentColaborations.Where(whereFunc).Select(x => x.RequestReceiver));
+			return friends;
+		}
+
+		/// <returns>All unconfirmed invitations to be colaborators</returns>
+		public IEnumerable<ColaborationRequest> GetUnconfirmedInvites()
+		{
+			var invites = AcceptedColaborations.Where(x => x.Status == ColaborationRequestStatus.Undefined);
+			return invites;
+		}
 	}
 }
