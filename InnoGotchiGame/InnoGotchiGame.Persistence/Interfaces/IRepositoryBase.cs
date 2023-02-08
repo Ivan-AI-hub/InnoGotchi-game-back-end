219bc37f8d19f6ebeea543c934cc7b7fdf44e7c9
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-02-08 12:55:10 +0300

    Create IRepositoryBase

--- InnoGotchiGame/InnoGotchiGame.Persistence/Interfaces/IRepositoryBase.cs
+++ InnoGotchiGame/InnoGotchiGame.Persistence/Interfaces/IRepositoryBase.cs
@@ -1,38 +1,36 @@
-﻿namespace InnoGotchiGame.Persistence.Interfaces
+﻿using System.Linq.Expressions;
+
+namespace InnoGotchiGame.Persistence.Interfaces
 {
-    public interface IRepository<T> where T : class
+    public interface IRepositoryBase<T> where T : class
     {
-        /// <returns>item from database with special id</returns>
-        public T? GetItemById(int id);
 
         /// <returns>true if the element exists, and false if not</returns>
         public bool IsItemExist(Func<T, bool> func);
-        /// <returns>true if the element exists, and false if not</returns>
-        public bool IsItemExist(int id);
 
         /// <param name="predicate">Special predicate for element search</param>
         /// <returns>The element, if it was found in the database or null</returns>
-        public T? GetItem(Func<T, bool> predicate);
+        public IQueryable<T> GetItemsByCondition(Expression<Func<T, bool>> predicate, bool trackChanges);
 
         /// <returns>queryable items from the database</returns>
-        public IQueryable<T> GetItems();
+        public IQueryable<T> GetItems(bool trackChanges);
 
         /// <summary>
-        /// Add item in database
+        /// Create item in database
         /// </summary>
         /// <returns>ID of the added element</returns>
-        public int Add(T item);
+        public void Create(T item);
 
         /// <summary>
         /// Update item in database
         /// </summary>
-        public void Update(int updatedId, T item);
+        public void Update(T item);
 
         /// <summary>
         /// Delete item from database
         /// </summary>
         /// <returns>true if complite and false if not</returns>
-        public bool Delete(int id);
+        public void Delete(T item);
 
         /// <summary>
         /// Save changes in database
