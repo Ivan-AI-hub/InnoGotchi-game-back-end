d6b2576a345142078a04481453b210b5f3d525f1
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-03-02 18:09:55 +0300

    update migration

--- InnoGotchiGame/InnoGotchiGame.Web/Migrations/20230302133127_IsAlivePetStateReduce.Designer.cs
+++ InnoGotchiGame/InnoGotchiGame.Web/Migrations/20230302133127_IsAlivePetStateReduce.Designer.cs
@@ -4,6 +4,7 @@ using InnoGotchiGame.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InnoGotchiGame.Web.Migrations
 {
     [DbContext(typeof(InnoGotchiGameContext))]
-    partial class InnoGotchiGameContextModelSnapshot : ModelSnapshot
+    [Migration("20230302133127_IsAlivePetStateReduce")]
+    partial class IsAlivePetStateReduce
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
